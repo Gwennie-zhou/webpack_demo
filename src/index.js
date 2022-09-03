aa9f30b27c66205ec4934bec29e6d5dbfbe1d396
Gwennie-zhou <Gwennie_zhou@126.com>
2022-09-03 14:58:27 +0800

    load css module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // 定义一个组件
 import _ from 'lodash'
+import './style.css'
+
 
 function component() {
     const element = document.createElement('div')
     element.innerHTML = _.join(['hello', 'world'], ' ')
+    element.classList.add('hello')
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,12 @@ module.exports = {
         path: path.resolve(__dirname, './dist')
     },
     mode: 'development',
-    
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
