b6b0598315da08bda2f22b94e4e0edb1c90287f2
Gwennie-zhou <Gwennie_zhou@126.com>
2022-09-03 15:26:07 +0800

    load images module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 // 定义一个组件
 import _ from 'lodash'
 import './style.css'
-
+import Bg from "./bg.jpg"
 
 function component() {
     const element = document.createElement('div')
     element.innerHTML = _.join(['hello', 'world'], ' ')
     element.classList.add('hello')
+
+    const bg = new Image()
+    bg.src = Bg
+    bg.classList.add("bg")
+    element.appendChild(bg)
+
     return element
 }
 
