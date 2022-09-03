0a9c2e22c559dc5c8523110ea16a14256aac169f
Gwennie-zhou <Gwennie_zhou@126.com>
2022-09-03 13:47:21 +0800

    webpack --init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// 定义一个组件
+import _ from 'lodash'
+
+function component() {
+    const element = document.createElement('div')
+    element.innerHTML = _.join(['hello', 'world'], ' ')
+    return element
+}
+
+document.body.appendChild(component())
